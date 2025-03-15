e6ed88ba8a604686e1bd4cff166f259151f6833c
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-03-15 11:37:49 +0100

    Update PostalCodesChecker.php

--- src/PostalCodesChecker.php
+++ src/PostalCodesChecker.php
@@ -4,63 +4,61 @@ namespace Kbarut\PostalCodes;
 
 class PostalCodesChecker
 {
-	/**
-	* validates or invalidates $input against the german zip codes listed in the 'src/zipcodes-de.json' file,
-	*
-	* @param string $input 
-	* @return bool
-	*/	
-	public function validate(string $input): bool
+   /**
+   * validates or invalidates $input against the german zip codes listed in the 'src/zipcodes-de.json' file,
+   *
+   * @param string $input 
+   * @return bool
+   */	
+    public function validate(string $input): bool
     {		
-		$cleaned_input = preg_replace('/\s+/','',$input);
+	$cleaned_input = preg_replace('/\s+/','',$input);
 		
-		if ( (ctype_digit($cleaned_input) === false) or (strlen($cleaned_input) != 5)  ) 
-			return false;
+	if ( (ctype_digit($cleaned_input) === false) or (strlen($cleaned_input) != 5)  ) 
+	   return false;
 						
-		$res = $this->checkPostalCode($cleaned_input);
+	$res = $this->checkPostalCode($cleaned_input);
 			
-		return $res;
-	}
+	return $res;
+    }
 	
-
-	/**
-	* Returns true if $cleaned_input matches any of the german zip codes listed in the 'src/zipcodes-de.json' file
-	* returns false otherwise.
-	*
-	* @param string $cleaned_input
-	* @return bool 
-	*/
-	public function checkPostalCode(string $cleaned_input): bool
+    /**
+    * Returns true if $cleaned_input matches any of the german zip codes listed in the 'src/zipcodes-de.json' file
+    * returns false otherwise.
+    *
+    * @param string $cleaned_input
+    * @return bool 
+    */
+    public function checkPostalCode(string $cleaned_input): bool
     {
-		$res = false;
+        $res = false;
 		
-		$data = [];
+	$data = [];
 		
-		$arr = [];
+	$arr = [];
 				
-		$jsonData = file_get_contents('zipcodes-de.json');
+	$jsonData = file_get_contents('zipcodes-de.json');
 		
-		$data = json_decode($jsonData,true);
+	$data = json_decode($jsonData,true);
 								
-		foreach( $data as $arr) {
+	foreach( $data as $arr) {
 			
-			foreach( $arr as $key => $value) {
+		foreach( $arr as $key => $value) {
 	
-				if  ( ($key === 'zipcode') && ($value === $cleaned_input) ) {
+			if  ( ($key === 'zipcode') && ($value === $cleaned_input) ) {
 					
-					$res = true;
+				$res = true;
 				
-					break;
-				
-				}	
-			}
-			
-			if ($res === true)
-				return $res;
-					
+				break;				
+			}	
 		}
-		
-		return $res;
+			
+		if ($res === true)
+			return $res;
+					
 	}
+		
+	return $res;
+   }
     		
 }
